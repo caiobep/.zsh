150fc55fd1835301f453727084b58af5a52d5eb4
Caio Amaral Corrêa <caiobep@me.com>
2018-06-16 15:04:28 -0300

    Pure theme instead of MaterialShell and new upgrades

---
+++ config/functions.zsh
@@ -0,0 +1,9 @@
+# Create a new directory and enter it
+function mkd() {
+	mkdir -p "$@" && cd "$_";
+}
+
+# kill all processes of a certain type
+function nuke {
+    kill `ps aux | grep $1 | awk '{print $2}'`
+}
---
+++ config/load.zsh
@@ -0,0 +1,42 @@
+# Make special variables duplicate-free.
+typeset -g -U path fpath cdpath
+
+# Set zsh custom autoload directory.
+fpath=($fpath "$ZDOTDIR/autoload")
+
+# Load zrecompile for zcompile.
+autoload -U zrecompile
+
+# Load prompt system.
+autoload -U promptinit
+
+# Load completion initialization to enable completion.
+autoload -U compinit
+compinit -d $ZDOTDIR/cache/.zcompdump # Completion cache file.
+
+# Load and register edit in $EDITOR function.
+autoload -U edit-command-line
+zle -N edit-command-line
+
+# Load custom hooks API.
+autoload -U add-zsh-hook
+
+# Load color variables.
+autoload -U colors && colors
+
+# Load and register ZLE surround plugin.
+autoload -U surround
+zle -N delete-surround surround
+zle -N add-surround surround
+zle -N change-surround surround
+
+# Load and register ZLE text objects for delimiters.
+autoload -U select-quoted
+zle -N select-quoted
+
+# Load and register ZLE text objects for pairs of brackets.
+autoload -U select-bracketed
+zle -N select-bracketed
+
+# Load extra features for completion.
+zmodload zsh/complist
